8474afe6184d0429f0a4f95f70befec019292f64
Douglas Gomes <douglasgomes.rr@gmail.com>
2026-01-14 01:30:56 -0300

    Refactor Vite configuration to integrate Tailwind CSS and Module Federation; remove PostCSS config and update global styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-const devPlugins: any[] = [];
-if (process.env.NODE_ENV !== 'production') devPlugins.push(vitePluginExposeEntry());
+import federation from '@originjs/vite-plugin-federation';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), ...devPlugins],
-  build: {
-    rollupOptions: {
-      input: 'src/bytebank-dashboard.tsx',
-      output: {
-        format: 'es',
-        entryFileNames: 'bytebank-dashboard.js',
+  plugins: [
+    react(),
+    tailwindcss(),
+    federation({
+      name: '@bytebank/dashboard',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './bytebank-dashboard': './src/bytebank-dashboard.tsx',
       },
-      external: ['react', 'react-dom', 'react-dom/client', 'react/jsx-runtime', 'single-spa', 'single-spa-react', 'scheduler'],
-      preserveEntrySignatures: 'strict',
-    },
-    outDir: 'dist',
+      shared: ['react', 'react-dom', 'react-router-dom'],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
   },
   server: {
     port: 9003,
@@ -27,20 +31,3 @@ export default defineConfig({
     cors: true,
   },
 });
-export function vitePluginExposeEntry() {
-  return {
-    name: 'vite-plugin-expose-bytebank-dashboard',
-    configureServer(server) {
-      server.middlewares.use((req, res, next) => {
-        const url = req.url || '';
-        if (url === '/bytebank-dashboard.js') {
-          const content = `export * from '/src/bytebank-dashboard.tsx';`;
-          res.setHeader('Content-Type', 'application/javascript');
-          res.end(content);
-          return;
-        }
-        next();
-      });
-    },
-  };
-}
