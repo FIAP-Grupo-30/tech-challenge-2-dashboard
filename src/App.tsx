a5162470a9947c0ce08c9c59883a886194be2d73
Douglas Gomes <douglasgomes.rr@gmail.com>
2026-01-17 00:13:53 -0300

    Refactor import statements in App.tsx and bytebank-dashboard.tsx for consistency and improved readability.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import Dashboard from './components/Dashboard';
+import Dashboard from "./components/Dashboard";
 
 function App() {
-  return (
-    <>
-      <main className="bg-[#e4e2e2] pt-10 pb-10 pl-4 pr-4">
-        {/* suas rotas */}
-        <div className="container mx-auto max-w-[1550px] bg-[#FFF] rounded-[12px] mx-auto p-12">
-          <Dashboard />
-        </div>
-      </main>
-    </>
-  );
+	return (
+		<>
+			<main className="bg-[#e4e2e2] pt-10 pb-10 pl-4 pr-4">
+				{/* suas rotas */}
+				<div className="container mx-auto max-w-[1550px] bg-[#FFF] rounded-[12px] mx-auto p-12">
+					<Dashboard />
+				</div>
+			</main>
+		</>
+	);
 }
 
 export default App;
