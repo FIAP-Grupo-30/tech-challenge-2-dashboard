44505e6134179d94a27e23362337a5b6449bf80f
Leandro de Freitas Rodrigues <leandrodfreitas@gmail.com>
2026-01-18 21:21:30 -0300

    Ajuste

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Dashboard from "./components/Dashboard";
 function App() {
 	return (
 		<main className="dash:bg-[#e4e2e2] dash:pt-10 dash:pb-10 dash:pl-4 dash:pr-4">
-			<div className="dash:container dash:mx-auto dash:max-w-[1550px] dash:bg-[#FFF] dash:rounded-[12px] dash:p-12">
+			<div className="dash:container dash:mx-auto dash:max-w-[1550px] dash:bg-[#FFF] dash:rounded-[12px] dash:p-10">
 				<Dashboard />
 			</div>
 		</main>
